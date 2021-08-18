47fb91813273f039151fea854d1c948643134264
Damjan Manojlović <damjan0032@gmail.com>
2021-08-19 00:08:25 +0200

    start second ui

---
+++ statistic-ui/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Chart/>
+    <h1>{{ msg }}</h1>
+    <Chart2/>
+  </div>
+</template>
+
+<script>
+import Chart from "./Chart";
+import Chart2 from "./Chart2";
+export default {
+  name: 'HelloWorld',
+  components: {Chart2, Chart},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
